f013adfae118f8fc1ff1d081101329f3ff4aa015
Андрей Овчаренко <andrey.ovcharenko@branderstudio.com>
2021-08-09 15:13:58 +0300

    config cluster

--- start.sh
+++ start.sh
@@ -3,9 +3,15 @@ mkdir -p data/mono
 mkdir -p data/rs0-1
 mkdir -p data/rs0-2
 mkdir -p data/rs0-3
+mkdir -p data/cf-0
+mkdir -p data/cf-1
+mkdir -p data/cf-2
 killall mongod
 sleep 20
 mongod --config ./config/mono.conf
 mongod --config ./config/rs0-1.conf
 mongod --config ./config/rs0-2.conf
 mongod --config ./config/rs0-3.conf
+mongod --config ./config/cf0.conf
+mongod --config ./config/cf1.conf
+mongod --config ./config/cf2.conf
